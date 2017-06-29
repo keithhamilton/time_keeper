d5e028887ca9f8fa832498d0971689fce19553b7
Keith Hamilton <the.keith.hamilton@gmail.com>
2017-06-29 05:32:00 -0700

    Include prod secret and use env vars

---
+++ config/prod.secret.exs
@@ -0,0 +1,18 @@
+use Mix.Config
+
+# In this file, we keep production configuration that
+# you likely want to automate and keep it away from
+# your version control system.
+#
+# You should document the content of this
+# file or create a script for recreating it, since it's
+# kept out of version control and might be hard to recover
+# or recreate for your teammates (or you later on).
+config :time_keeper, TimeKeeper.Endpoint,
+  secret_key_base: "QhhkTG7hFdTpkLMeNozi9U6tv9SWcAjPxyBDks9euy0q8NgITw/C6kzVd+rzP9uv"
+
+# Configure your database
+config :time_keeper, TimeKeeper.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  hostname: System.get_env("DATABASE_URL")
+  pool_size: 20

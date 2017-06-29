da9b0b27360f775920e3b227d20a314b5a1f6eec
Keith Hamilton <the.keith.hamilton@gmail.com>
2017-06-29 09:18:32 -0700

    Add more CRUD methods back into play

--- web/controllers/work_controller.ex
+++ web/controllers/work_controller.ex
@@ -168,6 +168,11 @@ defmodule TimeKeeper.WorkController do
     |> send_resp(200, resp)
   end
 
+  def switch_manual(conn, _params) do
+    buttons = Repo.all(from b in Button, select: b)
+    render(conn, "switch.html", buttons: buttons)
+  end
+
   def index(conn, _params) do
     work = Repo.all(Work)
     render(conn, "index.html", work: work)
@@ -194,45 +199,41 @@ defmodule TimeKeeper.WorkController do
         render(conn, "edit.html", work: work, changeset: changeset)
     end
   end
+
+  def new(conn, _params) do
+    changeset = Work.changeset(%Work{})
+    render(conn, "new.html", changeset: changeset)
+  end
+
+  def create(conn, %{"work" => work_params}) do
+    changeset = Work.changeset(%Work{}, work_params)
+
+    case Repo.insert(changeset) do
+      {:ok, _work} ->
+        conn
+        |> put_flash(:info, "Work created successfully.")
+        |> redirect(to: work_path(conn, :index))
+      {:error, changeset} ->
+        render(conn, "new.html", changeset: changeset)
+    end
+  end
+
+  def show(conn, %{"id" => id}) do
+    work = Repo.get!(Work, id)
+    render(conn, "show.html", work: work)
+  end
+
+
+
+  def delete(conn, %{"id" => id}) do
+    work = Repo.get!(Work, id)
+
+    # Here we use delete! (with a bang) because we expect
+    # it to always work (and if it does not, it will raise).
+    Repo.delete!(work)
+
+    conn
+    |> put_flash(:info, "Work deleted successfully.")
+    |> redirect(to: work_path(conn, :index))
+  end
 end
-
-
-  #
-  # def new(conn, _params) do
-  #   changeset = Work.changeset(%Work{})
-  #   render(conn, "new.html", changeset: changeset)
-  # end
-  #
-  # def create(conn, %{"work" => work_params}) do
-  #   changeset = Work.changeset(%Work{}, work_params)
-  #
-  #   case Repo.insert(changeset) do
-  #     {:ok, _work} ->
-  #       conn
-  #       |> put_flash(:info, "Work created successfully.")
-  #       |> redirect(to: work_path(conn, :index))
-  #     {:error, changeset} ->
-  #       render(conn, "new.html", changeset: changeset)
-  #   end
-  # end
-  #
-  # def show(conn, %{"id" => id}) do
-  #   work = Repo.get!(Work, id)
-  #   render(conn, "show.html", work: work)
-  # end
-  #
-
-  #
-
-  #
-  # def delete(conn, %{"id" => id}) do
-  #   work = Repo.get!(Work, id)
-  #
-  #   # Here we use delete! (with a bang) because we expect
-  #   # it to always work (and if it does not, it will raise).
-  #   Repo.delete!(work)
-  #
-  #   conn
-  #   |> put_flash(:info, "Work deleted successfully.")
-  #   |> redirect(to: work_path(conn, :index))
-  # end

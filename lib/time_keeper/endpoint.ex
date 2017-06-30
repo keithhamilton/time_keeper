8204cd4eaaabd4b542fea267700a770137b4b8cb
Keith Hamilton <the.keith.hamilton@gmail.com>
2017-06-30 05:58:30 -0700

    Enable static audio

--- lib/time_keeper/endpoint.ex
+++ lib/time_keeper/endpoint.ex
@@ -9,7 +9,7 @@ defmodule TimeKeeper.Endpoint do
   # when deploying your static files in production.
   plug Plug.Static,
     at: "/", from: :time_keeper, gzip: false,
-    only: ~w(css fonts images js favicon.ico robots.txt)
+    only: ~w(css fonts audio images js favicon.ico robots.txt)
 
   # Code reloading can be explicitly enabled under the
   # :code_reloader configuration of your endpoint.

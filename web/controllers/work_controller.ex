00126847ed1f6e160564002a351d498b43be5b9b
Keith Hamilton <the.keith.hamilton@gmail.com>
2017-07-03 15:45:55 -0700

    Begin work/date inversion for reporting

--- web/controllers/work_controller.ex
+++ web/controllers/work_controller.ex
@@ -6,25 +6,48 @@ defmodule TimeKeeper.WorkController do
   alias TimeKeeper.User
   alias TimeKeeper.Work
 
+  # def aggregate_time([first_entry|time_entries], aggregate) do
+  #   job_code = first_entry.job_code
+  #   date_string = Date.to_string(first_entry.date)
+  #
+  #   if Map.has_key?(aggregate, job_code) do
+  #     date_hash = Map.get(aggregate, date_string)
+  #
+  #     if Map.has_key?(date_hash, first_entry.job_code) do
+  #       total_time = Map.get(date_hash, first_entry.job_code) + first_entry.time_spent
+  #       new_date_hash = Map.put(date_hash, first_entry.job_code, total_time)
+  #       new_aggregate = Map.put(aggregate, date_string, new_date_hash)
+  #       aggregate_time(time_entries, new_aggregate)
+  #     else
+  #       new_date_hash = Map.put(date_hash, first_entry.job_code, first_entry.time_spent)
+  #       new_aggregate = Map.put(aggregate, date_string, new_date_hash)
+  #       aggregate_time(time_entries, new_aggregate)
+  #     end
+  #   else
+  #     date_hash = %{first_entry.job_code => first_entry.time_spent}
+  #     new_aggregate = Map.put(aggregate, date_string, date_hash)
+  #     aggregate_time(time_entries, new_aggregate)
+  #   end
+  # end
+
   def aggregate_time([first_entry|time_entries], aggregate) do
-    date_string = Date.to_string(first_entry.date)
+    job_code = first_entry.job_code
 
-    if Map.has_key?(aggregate, date_string) do
-      date_hash = Map.get(aggregate, date_string)
+    if Map.has_key?(aggregate, job_code) do
+      job_hash = Map.get(aggregate, job_code)
 
-      if Map.has_key?(date_hash, first_entry.job_code) do
-        total_time = Map.get(date_hash, first_entry.job_code) + first_entry.time_spent
-        new_date_hash = Map.put(date_hash, first_entry.job_code, total_time)
-        new_aggregate = Map.put(aggregate, date_string, new_date_hash)
+      if Map.has_key?(job_hash, first_entry.date) do
+        total_time = Map.get(job_hash, first_entry.date) + first_entry.time_spent
+        new_job_hash = Map.put(job_hash, first_entry.date, total_time)
+        new_aggregate = Map.put(aggregate, job_code, new_job_hash)
         aggregate_time(time_entries, new_aggregate)
       else
-        new_date_hash = Map.put(date_hash, first_entry.job_code, first_entry.time_spent)
-        new_aggregate = Map.put(aggregate, date_string, new_date_hash)
-        aggregate_time(time_entries, new_aggregate)
+        new_job_hash = Map.put(job_hash, first_entry.date, first_entry.time_spent)
+        new_aggregate = Map.put(aggregate, job_code, new_job_hash)
       end
     else
-      date_hash = %{first_entry.job_code => first_entry.time_spent}
-      new_aggregate = Map.put(aggregate, date_string, date_hash)
+      job_hash = %{first_entry.date => first_entry.time_spent}
+      new_aggregate = Map.put(aggregate, job_code, job_hash)
       aggregate_time(time_entries, new_aggregate)
     end
   end
@@ -42,7 +65,7 @@ defmodule TimeKeeper.WorkController do
       |> Float.round(2)
 
     calendar_date = NaiveDateTime.to_date(work_object.inserted_at)
-    %{job_code: work_object.job_code, date: calendar_date, time_spent: hours}
+    %{job_code: work_object.job_code, date: Date.to_string(calendar_date), time_spent: hours}
   end
 
   def close(conn, work_object) do

3c22ed97e546b6764bad5465b0de24d3441ef3a0
Keith Hamilton <the.keith.hamilton@gmail.com>
2017-07-04 14:45:49 -0700

    Update redirect path

--- web/redirector.ex
+++ web/redirector.ex
@@ -1,5 +1,5 @@
 defmodule TimeKeeper.Redirector do
   use Plug.Redirect
 
-  redirect("/work/:start_date/:end_date", "/work/:start_date/:end_date/false")
+  redirect("/work/:start_date/:end_date", "/work/:start_date/:end_date/browser")
 end

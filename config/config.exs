624488ab19d89204d7ed79a94e2948946b4c115f
Keith Hamilton <the.keith.hamilton@gmail.com>
2017-07-06 08:26:12 -0700

    Add addict config

--- config/config.exs
+++ config/config.exs
@@ -25,3 +25,13 @@ config :logger, :console,
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{Mix.env}.exs"
+
+config :addict,
+  secret_key: "24326224313224676a54536f52626c4275456d53674f38767271334175",
+  extra_validation: fn ({valid, errors}, user_params) -> {valid, errors} end, # define extra validation here
+  user_schema: TimeKeeper.User,
+  repo: TimeKeeper.Repo
+  # from_email: "no-reply@example.com", # CHANGE THIS
+  # mailgun_domain: "",
+  # mailgun_key: "",
+  # mail_service: :mailgun

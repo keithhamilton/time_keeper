88ac0531d2f50d4a716d9d22e5aac331e21b02f8
Keith Hamilton <the.keith.hamilton@gmail.com>
2017-07-06 08:30:29 -0700

    Update module

--- web/services/time_services.ex
+++ web/services/time_services.ex
@@ -8,26 +8,20 @@ defmodule TimeKeeper.TimeServices do
   def current_pay_period(date) do
     case date.day do
       x when x <= 15 ->
-        first_of_period = date
+        [date
         |> Timex.shift(days: (date.day - 1) * -1)
-        |> Timex.set(hour: 0, minute: 0, second: 0)
-
-        last_of_period = date
+        |> Timex.set(hour: 0, minute: 0, second: 0),
+        date
         |> Timex.shift(days: 15 - date.day)
-        |> Timex.set(hour: 23, minute: 59, second: 59)
-
-        [first_of_period, last_of_period]
+        |> Timex.set(hour: 23, minute: 59, second: 59)]
       _ ->
         last_of_month = date |> Timex.days_in_month
-        first_of_period = date
-        |> Times.shift(days: (date.day - 16) * -1)
-        |> Timex.set(hour: 0, minute: 0, second: 0)
-
-        last_of_period = date
+        [date
+        |> Timex.shift(days: (date.day - 16) * -1)
+        |> Timex.set(hour: 0, minute: 0, second: 0),
+        date
         |> Timex.shift(days: last_of_month - date.day)
-        |> Timex.set(hour: 23, minute: 59, second: 59)
-
-        [first_of_period, last_of_period]
+        |> Timex.set(hour: 23, minute: 59, second: 59)]
     end
   end
 end
